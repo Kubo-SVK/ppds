7ac4f43cbf78233a37c157262a8da870e53b0924
Jakub Sip <jakubsip52@gmail.com>
2021-03-14 00:24:47 +0100

    feat(Zadanie 4): Pridanie oneskorení
    Pridanie oneskorení simulujúcich prácu senzorov/monitorov

--- Zadanie 4/Atomka_2.py
+++ Zadanie 4/Atomka_2.py
@@ -35,8 +35,9 @@ def init():
  
     for monitor_id in range(8):
         threads.append(Thread(monitor, monitor_id, turniket, validData, ls_monitor, accessData))
-    for cidlo_id in range(3):
-        threads.append(Thread(cidlo, cidlo_id, turniket, validData, ls_cidlo, accessData))
+    for cidlo_id in range(2):
+        threads.append(Thread(cidlo, cidlo_id, turniket, validData, ls_cidlo, accessData, [10, 20]))
+    threads.append(Thread(cidlo, 2, turniket, validData, ls_cidlo, accessData, [20, 25]))
         
     for th in threads:
         th.join()
@@ -48,19 +49,22 @@ def monitor(monitor_id, turniket, validData,ls_monitor, accessData):
         sleep(0.5)
  
         turniket.wait()
+        sleep(randint(40, 50) / 1000)
         pocet_citajucich_monitorov = ls_monitor.lock(accessData)
         turniket.signal()
  
         print(f'monit "{monitor_id:02d}": pocet_citajucich_monitorov={pocet_citajucich_monitorov:02d}')
         ls_monitor.unlock(accessData)
  
-def cidlo(cidlo_id, turniket, validData, ls_cidlo, accessData):
+def cidlo(cidlo_id, turniket, validData, ls_cidlo, accessData, wr):
     while True:
+        sleep(randint(50, 60) / 1000)
+        
         turniket.wait()
         turniket.signal()
  
         pocet_zapisujucich_cidiel = ls_cidlo.lock(accessData)
-        trvanie_zapisu = randint(50, 60)/1000
+        trvanie_zapisu = randint(wr[0], wr[1])/1000
         
         print(f'cidlo "{cidlo_id:02d}":  pocet_zapisujucich_cidiel={pocet_zapisujucich_cidiel:02d}, trvanie_zapisu={trvanie_zapisu:0.3f}')
         sleep(trvanie_zapisu)
