a0fe312f55a5850d333f6a3f4d5aace42e47fef9
Jakub Sip <jakubsip52@gmail.com>
2021-03-22 14:59:28 +0100

    style(Zadanie 5): Úprava formátovania na pep8
    Úprava formátovania na pep8

--- Zadanie 5/Zadanie_5.py
+++ Zadanie 5/Zadanie_5.py
@@ -1,7 +1,7 @@
 from fei.ppds import Semaphore, Mutex, Thread, print
 from random import randint
 from time import sleep
- 
+
 """M a N su parametre modelu, nie synchronizacie ako takej.
 Preto ich nedavame do zdielaneho objektu.
     M - pocet porcii misionara, ktore sa zmestia do hrnca.
@@ -11,15 +11,15 @@ Preto ich nedavame do zdielaneho objektu.
 M = 10
 N = 5
 C = 3
- 
- 
+
+
 class SimpleBarrier:
     def __init__(self, N):
         self.N = N
         self.mutex = Mutex()
         self.cnt = 0
         self.sem = Semaphore(0)
- 
+
     def wait(self,
              print_str,
              savage_id,
@@ -36,8 +36,8 @@ class SimpleBarrier:
             self.sem.signal(self.N)
         self.mutex.unlock()
         self.sem.wait()
- 
- 
+
+
 class Shared:
     def __init__(self):
         self.mutex = Mutex()
@@ -51,19 +51,19 @@ class Shared:
         self.barrier1 = SimpleBarrier(N)
         self.barrier2 = SimpleBarrier(N)
         self.pot_ready = Semaphore(0)
- 
- 
+
+
 def get_serving_from_pot(savage_id, shared):
     print("divoch %2d: beriem si porciu" % savage_id)
     shared.servings -= 1
- 
- 
+
+
 def eat(savage_id):
     print("divoch %2d: hodujem" % savage_id)
     # Zjedenie porcie misionara nieco trva...
     sleep(0.2 + randint(0, 3) / 10)
- 
- 
+
+
 def savage(savage_id, C, shared):
     while True:
 
@@ -74,7 +74,7 @@ def savage(savage_id, C, shared):
         shared.barrier2.wait("divoch %2d: uz sme vsetci, zaciname vecerat",
                              savage_id,
                              print_last_thread=True)
- 
+
         # Nasleduje klasicke riesenie problemu hodujucich divochov.
         shared.mutex.lock()
         print("divoch %2d: pocet zostavajucich porcii v hrnci je %2d" %
@@ -85,11 +85,11 @@ def savage(savage_id, C, shared):
             shared.full_pot.wait()
         get_serving_from_pot(savage_id, shared)
         shared.mutex.unlock()
- 
+
         eat(savage_id)
- 
- 
-def put_servings_in_pot(id, M, shared): 
+
+
+def put_servings_in_pot(id, M, shared):
     while True:
         shared.cooking.lock()
         if(shared.active_cooks == M):
@@ -101,13 +101,13 @@ def put_servings_in_pot(id, M, shared):
         # navarenie jedla tiez cosi trva...
         sleep(0.4 + randint(0, 2) / 10)
         shared.servings += 1
- 
- 
+
+
 def cook(id, M, C, shared):
     while True:
         shared.empty_pot.wait()
         put_servings_in_pot(id, M, shared)
-        
+
         shared.refill.lock()
         print(f'kuchar {id}: hotovo')
         shared.cooks_done += 1
@@ -116,20 +116,20 @@ def cook(id, M, C, shared):
             shared.cooks_done = 0
             shared.full_pot.signal()
         shared.refill.unlock()
- 
- 
+
+
 def init_and_run(N, M, C):
     threads = list()
     shared = Shared()
     for savage_id in range(0, N):
         threads.append(Thread(savage, savage_id, C, shared))
-    
+
     for cook_id in range(0, C):
         threads.append(Thread(cook, cook_id, M, C, shared))
- 
+
     for t in threads:
         t.join()
- 
- 
+
+
 if __name__ == "__main__":
     init_and_run(N, M, C)
